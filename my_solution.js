a658347dec2ce0702b808ace22629761cbd4625a
Ashley <ashleywong678@gmail.com>
2021-07-18 20:32:53 -0400

    Update my_solution.js

--- my_solution.js
+++ my_solution.js
@@ -1,7 +1,17 @@
+class Tree {
+    constructor (value = null, left = null, right = null) {
+        this.value = value
+        this.left = left
+        this.right = right
+    }    
+}
+    
 function sortedArrayToBST(array) {
     if(!array.length) return null
     const mid = Math.floor((array.length - 1) / 2)ew Tree(array[mid])
         
+    const root = new Tree(array[mid])
+    
     root.left = sortedArrayToBST(array.slice(0, mid))
     root.right = sortedArrayToBST(array.slice(mid + 1))
     
